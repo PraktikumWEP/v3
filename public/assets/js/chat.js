23cd54b98c51fc84aaf5d1ca4f3d18f9aa24710e
MysterionAutotronic <76444188+MysterionAutotronic@users.noreply.github.com>
2022-10-26 14:45:10 +0200

    started c, html still missing, cfg updated

---
+++ public/assets/js/chat.js
@@ -0,0 +1,58 @@
+let COLLECTION_ID;
+let TOM_TOKEN; // initialize empty for global scope
+let JERRY_TOKEN;
+
+// fetch config data
+fetch('./cfg.json')
+.then(response => response.json()) // get object from response
+.then(data => {
+    COLLECTION_ID = data.COLLECTION_ID;
+    TOM_TOKEN = data.tom.token;
+    JERRY_TOKEN = data.jerry.token;
+})
+.catch(err => { 
+    console.error(err);
+});
+
+// initial load
+loadMessages();
+
+// refresh
+window.setInterval(e => {
+    loadMessages();
+}, 1000);
+
+function loadMessages(token) {
+    // vars for request
+    let uri = "https://online-lectures-cs.thi.de/chat/" + COLLECTION_ID + "/message/" + token;
+
+    // vars for data
+    let messages = [];
+    
+    // request
+    try {
+        fetch(uri)
+        .then(response => response.json())
+        .then(data => {
+            messages = data;
+        });
+    }
+    catch(e) {
+        console.error(e);
+    }
+    finally {
+        showMessages(messages);
+    }
+}
+
+function showMessages(messages) {
+    for(let i = 0; i < messages.lenght; i++) {
+        addMessage(messages[i]);
+    }
+}
+
+function addMessage(msg) {
+    let sender = msg.from;
+    let content = msg.msg;
+    let time = msg.time;
+}
